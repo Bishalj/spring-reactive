e5cc4db443d25aac0898ffeb6a1e7306ca69b1d2
Bishal Jaiswal <jaiswalbishal3@gmail.com>
2019-06-05 02:26:37 +0530

    Added test case

--- src/test/java/com/springreactivelearning/SpringReactiveLearningApplicationTests.java
+++ src/test/java/com/springreactivelearning/SpringReactiveLearningApplicationTests.java
@@ -2,13 +2,15 @@ package com.springreactivelearning;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringReactiveLearningApplicationTests {
-
+public class SpringReactiveLearningApplicationTests{
 	@Test
 	public void contextLoads() {
 	}
