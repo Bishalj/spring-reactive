ec86ec82045025fbeacc6c4a61d8827e6f9c6278
Bishal Jaiswal <jaiswalbishal3@gmail.com>
2019-06-06 02:16:14 +0530

    Added test cases for flux api

--- src/test/java/com/springreactivelearning/controller/FluxAndMonoControllerTest.java
+++ src/test/java/com/springreactivelearning/controller/FluxAndMonoControllerTest.java
@@ -7,9 +7,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
+import org.springframework.test.web.reactive.server.ExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @WebFluxTest
@@ -39,5 +46,44 @@ public class FluxAndMonoControllerTest {
                     .expectNext(4)
                     .verifyComplete();
         }
+    @Test
+    public void flux_approach2(){
 
+            webTestClient.get().uri("/flux")
+                    .accept(MediaType.APPLICATION_JSON_UTF8)
+                    .exchange()
+                    .expectStatus().isOk()
+                    .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                    .expectBodyList(Integer.class)
+                    .hasSize(4);
+        }
+
+    @Test
+    public void flux_approach3(){
+
+        List<Integer> expectedList = Arrays.asList(1,2,3,4);
+        EntityExchangeResult<List<Integer>> listResult = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Integer.class)
+                .returnResult();
+
+        assertEquals(expectedList, listResult.getResponseBody());
     }
+
+    @Test
+    public void flux_approach4(){
+        List<Integer> expectedList = Arrays.asList(1,2,3,4);
+
+        webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Integer.class)
+                .consumeWith((response) ->
+                        assertEquals(expectedList, response.getResponseBody()));
+    }
+}
