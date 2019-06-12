c16e1a1be35d7213cfe7466256c139c6137c5f87
Bishal Jaiswal <jaiswalbishal3@gmail.com>
2019-06-13 01:34:04 +0530

    Added item reactive mongo repository

--- src/main/java/com/springreactivelearning/SpringReactiveLearningApplication.java
+++ src/main/java/com/springreactivelearning/SpringReactiveLearningApplication.java
@@ -1,19 +1,19 @@
 package com.springreactivelearning;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import javax.validation.Valid;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
+
 public class SpringReactiveLearningApplication {
 
-    @Value("${data.mongodb.database}")
-    static  String db;
 	public static void main(String[] args) {
-	    System.out.println("DB" + db);
+
+
 		SpringApplication.run(SpringReactiveLearningApplication.class, args);
 	}
 
