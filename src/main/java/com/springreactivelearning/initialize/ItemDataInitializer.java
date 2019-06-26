102036e10ab51e4a6073e1df48951fa2a136d08f
bishal.j@turtlemint.com <jaiswalbishal3@gmail.com>
2019-06-27 00:51:50 +0530

    Added insert itemCapped method to insert data infinitely

--- src/main/java/com/springreactivelearning/initialize/ItemDataInitializer.java
+++ src/main/java/com/springreactivelearning/initialize/ItemDataInitializer.java
@@ -2,6 +2,7 @@ package com.springreactivelearning.initialize;
 
 import com.springreactivelearning.document.Item;
 import com.springreactivelearning.document.ItemCapped;
+import com.springreactivelearning.repository.ItemCappedReactiveRepository;
 import com.springreactivelearning.repository.ItemReactiveRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -11,6 +12,7 @@ import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Flux;
 
+import java.time.Duration;
 import java.util.Arrays;
 import java.util.List;
 
@@ -22,13 +24,24 @@ public class ItemDataInitializer implements CommandLineRunner {
     ItemReactiveRepository itemReactiveRepository;
 
     @Autowired
+    ItemCappedReactiveRepository itemCappedReactiveRepository;
+
+    @Autowired
     MongoOperations mongoOperations;
 
     @Override
     public void run(String... args) throws Exception {
         initializeDataSetup();
         createItemCapped();
+        dataSetupForCappedCollection();
+    }
 
+    private void dataSetupForCappedCollection() {
+        Flux<ItemCapped> itemCappedFlux = Flux.interval(Duration.ofSeconds(1))
+                .map( i -> new ItemCapped(null, "Random Item" + i, 100.0 + i)).log();
+
+        itemCappedReactiveRepository.insert(itemCappedFlux)
+        .subscribe(res -> System.out.println("Inserted item is "+ res.getDescription()));
     }
 
     private void createItemCapped() {
